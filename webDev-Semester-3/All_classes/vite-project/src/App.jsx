c6b0de0381c8f8b8e3fa685e285e60770d7a9df4
Ankit Kumar <ankitkumar17541@gmail.com>
2026-08-24 10:39:48 +0530

    24 aug

--- webDev-Semester-3/All_classes/vite-project/src/App.jsx
+++ webDev-Semester-3/All_classes/vite-project/src/App.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
-
-
+import { useState } from 'react'
+import axios from 'axios';
 import { useEffect } from 'react'
 const App = () => {
+  let [apidata,setapidata]=useState([])
   useEffect(()=>{
-    fetch("http://localhost:3000/").then((res)=>{
-      return res.json()
-    }).then((data)=>{
-      console.log(data);
-    })
+
+    async function api() {
+      let res =await axios.get("http://localhost:3000/")
+      console.log(res.data);
+      setapidata(res.data)
+    }
+    api()
+    // fetch("http://localhost:3000/").then((res)=>{
+    //   return res.json()
+    // }).then((data)=>{
+    //   setapidata(data)
+    // })
   },[])
   
   return (<>
-    
+    {
+      apidata.map((val)=>{
+        return(<>
+        <div key={val.id}>
+            <h1>ID: {val.id}</h1>
+            <h1>Brand: {val.brand}</h1>
+            <h1>Name: {val.name}</h1>
+            <h1>price: {val.price}</h1>
+          </div>
+        </>)
+      })
+    }
     </>
   )
 }
