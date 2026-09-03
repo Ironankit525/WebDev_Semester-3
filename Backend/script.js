345d974688b655a9c8d4f811b0e9263dccb547cd
Ankit Kumar <ankitkumar17541@gmail.com>
2026-09-03 09:52:43 +0530

    3 sept

--- Backend/script.js
+++ Backend/script.js
@@ -264,16 +264,28 @@ let auth=(req,res,next)=>{
    }
   let decode=  jwt.verify(token,"token hai bhai")
   console.log(decode,"isse");
+
+
+  req.user=decode;
+
   next()
 }
+   
+let roleCheck=(role)=>{
+   return(req,res,next)=>{
+      if(req.user.role!==role){
+         return res.send("who the hell are you ")
+      }
+      next()
+   }
+}
 
-
-app.get("/api",auth,(req,res)=>{
+app.get("/api",auth, roleCheck,(req,res)=>{
    res.send("api requst hai")
 
 })
 
- 
+   
 
 app.listen(3000,()=>{
   console.log("server is runnung ....... ")
